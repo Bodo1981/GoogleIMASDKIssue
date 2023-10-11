004a66691d10ee511c17eba578dd59b5c435c8b7
Christian Bahl <christian@tickaroo.com>
2023-10-11 16:47:14 +0200

    first commit

---
+++ app/src/main/java/com/jetbrains/simplelogin/googleimasample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.simplelogin.googleimasample
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.textView).setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java),
+                ActivityOptions.makeCustomAnimation(
+                    this,
+                    R.anim.kicker_slide_right_in,
+                    R.anim.kicker_slide_left_out
+                ).toBundle()
+            )
+
+        }
+    }
+}
